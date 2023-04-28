0eaf224e78040138b2035550f0a21fb052e33995
Thando224 <104420388+Thando224@users.noreply.github.com>
2023-04-28 06:51:10 +0200

    Dragged my Recipe program folder
    This is to allow me to do my First commit and if I wish to make any changes or add something i will not have to face errors as I did from the local machine.

---
+++ Recipe/Part1/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part1
+{
+    internal class Program
+    {
+        public void CreateRecipe() //I created a method CreateRecipe() inside the class which prompts the user for all the recipe information required. This method will be inherited by any instance of this class.
+        {
+            Console.WriteLine("*********************************************************************");
+            Console.WriteLine("Welcome to TidoRecipes!");
+            Console.WriteLine("*********************************************************************");
+
+
+            Console.WriteLine();
+            // Get the number of ingredients
+            Console.Write("Enter the number of ingredients: ");
+            recipeVar.numOfIngredients = int.Parse(Console.ReadLine());
+
+            //declare size of array according to numOfIngredients
+
+            recipeVar.ingArray = new string[recipeVar.numOfIngredients];
+
+            //the array value is now known
+            Console.WriteLine("size of array=" + recipeVar.ingArray.Length);
+            Console.WriteLine();
+
+
+            //1.b the user inputs the names of the ingredients
+            for (int i = 0; i < recipeVar.numOfIngredients; i++)
+            {
+                Console.WriteLine("Enter the name");
+                recipeVar.ingName = Console.ReadLine();
+                //save to array
+                recipeVar.ingArray[i] = recipeVar.ingName;
+
+                // the user inputs the quantity of that ingredient 
+                recipeVar.quantityArray = new double[recipeVar.numOfIngredients];
+                Console.WriteLine("Enter quantity");
+                recipeVar.ingQuantity = int.Parse(Console.ReadLine());
+                recipeVar.quantityArray[i] = recipeVar.ingQuantity;
+
+                //user inputs the unit measurement of the ingredient
+                recipeVar.unitArray = new string[recipeVar.numOfIngredients];
+                Console.WriteLine("Enter the unit");
+                recipeVar.ingUnit = Console.ReadLine();
+                recipeVar.unitArray[i] = recipeVar.ingUnit;
+
+
+                Console.WriteLine("End of ingredient " + (i + 1));
+                Console.WriteLine();
+
+            }
+            //1.c user inputs the number of steps for the ingredients
+            Console.WriteLine("Enter the amount of steps");
+            recipeVar.numOfSteps = int.Parse(Console.ReadLine());//this gives us the index value
+
+            recipeVar.stepsArray = new string[recipeVar.numOfSteps];
+
+
+            //the array value is now known
+            Console.WriteLine("size of array = " + recipeVar.stepsArray.Length);
+            Console.WriteLine();
+
+
+            //1.d user inputs the description of each step 
+            for (int i = 0; i < recipeVar.numOfSteps; i++)
+            {
+                recipeVar.descriptArray = new string[recipeVar.numOfSteps];
+                Console.WriteLine("Enter a brief description of step " + (i + 1));
+                recipeVar.descriptionOfSteps = Console.ReadLine();
+                recipeVar.descriptArray[i] = recipeVar.descriptionOfSteps; //store in array
+               
+            }
+        }
+
+
+            static void Main(string[] args)
+            {
+            Program create = new Program(); //I made use of the new keyword to create an instance of the class Program.I named this instance create.
+            create.CreateRecipe();  //I invoke the method CreateRecipe() using the newly created instance.
+
+
+            while (true)
+            {
+                Console.WriteLine("\n\nWhat would you like to do?");
+                Console.WriteLine("1. Display the recipe");
+                Console.WriteLine("2. Scale the recipe");
+                Console.WriteLine("3. Reset the quantities");
+                Console.WriteLine("4. Clear all data and enter new recipe details");
+                Console.WriteLine("5. Exit");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        recipeVar.printFullRecipe();
+                        break;
+
+                    case "2":
+                        recipeVar.Scalequantities();
+                        break;
+
+                    case "3":
+                        recipeVar.ResetQuantities();
+                        break;
+
+                    case "4":
+                         recipeVar.ClearRecipe();
+                        Program create1 = new Program(); //I made use of the new keyword to create an instance of the class Program.I named this instance create1.
+                        create1.CreateRecipe();  //I invoke the method CreateRecipe() using the newly created instance.
+                        break;
+                   
+                    case "5":
+                        recipeVar.ExistApp();
+                        break;
+                }
+
+
+            }
+        }
+    }
+}
---
+++ Recipe/Part1/recipeVar.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Channels;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Part1
+{
+    internal class recipeVar
+    {
+        //arrays are declared here
+        public static string[] ingArray; // this will store the names of all the ingredients entered by the user
+        public static string[] stepsArray; // this stores the number of steps entered by the user
+        public static double[] quantityArray; // this stores the quantity of the ingrdient name entered by user
+        public static string[] unitArray;// this will store the unit of measurement for the ingredient name
+        public static string[] descriptArray; // this stores the description of each step that depent on the number of steps the user entered
+
+        //storing all varibales used in porogrm
+        public static int numOfIngredients = 0; // this variabl stores the number of ingredients the user enters
+        public static string ingName = ""; // this variable stores the name of each ingredient
+        public static int numOfSteps = 0; //this variable stores the number of steps required for the recipe 
+       public static double ingQuantity = 0.0; // its called ingQuantity because it refers to the ingredient quantity entered by the user
+        public static string ingUnit = ""; // its called ingUnit because it refers to the ingrdient unit of measurement and stores the unit entered
+        public static string descriptionOfSteps = ""; // this stores the description of each step needed. 
+        
+        
+        //Methods
+
+        public static void printFullRecipe()  //this method displays the all the input the user entered of which was stored in memory using arrays. i also made use of a for loop to store and read the array index
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();  
+            Console.WriteLine("Here are your full recipe details:");
+            Console.WriteLine("Number of Ingredients : " +numOfIngredients);
+            for (int i = 0; i < numOfIngredients; i++)
+            {
+                Console.WriteLine("Each Ingredient consists of the Name: " + ingArray[i] + " " + "the Quntity: " + quantityArray[i] + " " + "Unit of Measurement is :" + unitArray[i]);
+            }
+
+            Console.WriteLine("Number of steps: " + numOfSteps);
+            for (int i = 0; i < numOfSteps; i++)
+               
+            {
+                Console.WriteLine("Description of each step: " + descriptArray[i]);
+            }
+         Console.ResetColor();
+        }
+
+        
+        public static void Scalequantities() // this method will ask to the user if they wish to scale up, should they agree it will store their input in the variable name scalename of which will be used to compare in the if statement
+        {
+        Console.WriteLine("Do you want to scale up your ingredients? Enter (1) Yes or Enter (2) No;");
+        int Scaleanswer = int.Parse(Console.ReadLine());
+
+            if (Scaleanswer == 1)
+            {
+                Console.WriteLine("Enter one of the scales between 0.5, 2 and 3");
+                double factor = Convert.ToDouble(Console.ReadLine());   
+                factor *= quantityArray[0]; //the *= operator multiples the value of the array index stored by the value of the variable on the left
+                Console.WriteLine(factor);
+                
+            }
+        }
+
+        public static void ResetQuantities() //this method sets all the valu
+        {
+         Console.WriteLine("Do you want reset the quanties? Enter (1) Yes or Enter (2) No;");
+         int Resetanswer = int.Parse(Console.ReadLine());
+
+            if (Resetanswer == 1) 
+            { 
+            ingQuantity = 0.0;  
+            }
+        }
+
+        public static void ClearRecipe() //this method clears out all the input entered from the beginning of the program and clears out anything stored in the arrays so that a new recipe can be entered
+        { 
+
+            for(int i = 0; i < ingArray.Length; i++)
+            {
+               ingArray[i] = null;
+                unitArray[i] = null;
+                quantityArray[i] = 0;
+            }
+
+          for (int i = 0; i < stepsArray.Length; i++)
+          {
+               stepsArray[i] = null;
+               descriptArray[i] = null;        
+           }
+
+           
+        }
+
+        public static void ExistApp() // this method terminates the application immediately
+        { 
+        Environment.Exit(0);    
+        }
+    }
+
+}
